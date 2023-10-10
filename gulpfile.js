e4c6b66b5a41dfaa6b5f7b08101044b9a5489d5c
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2023-10-10 19:46:27 -0300

    :heavy_plus_sign: dependencies added

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const imagemin = require('gulp-imagemin')
+
+function compilaSass() {
+  return gulp.src('./source/sass/*.scss')
+  .pipe(sass({outputStyle:'compressed'}))
+  .pipe(gulp.dest('./dist/css/'));
+} 
+function comprimeImagens() {
+  return gulp.src('./source/images/**/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('./dist/images/'));
+} 
+
+exports.default = gulp.parallel(compilaSass, comprimeImagens);
+
+exports.watch = function(){
+  gulp.watch('./source/sass/*.scss', gulp.parallel(compilaSass))
+}
